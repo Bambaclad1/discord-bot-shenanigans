7a5c870b4752bfa209c1f40014e9a796060c6ea2
Ramandeep Singh <67043806+Bambaclad1@users.noreply.github.com>
2026-05-09 23:03:52 +0200

    Create neofetch.js

---
+++ commands/utility/neofetch.js
@@ -0,0 +1,23 @@
+// src/commands/utility/neofetch.js
+const { SlashCommandBuilder, codeBlock } = require('discord.js');
+const { exec } = require('child_process');
+const { promisify } = require('util');
+
+const execAsync = promisify(exec);
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('neofetch')
+    .setDescription('Shows system info'),
+
+  async execute(interaction) {
+    await interaction.deferReply();
+
+    try {
+      const { stdout } = await execAsync('neofetch --stdout');
+      await interaction.editReply(codeBlock('bash', stdout));
+    } catch (err) {
+      await interaction.editReply('Failed to run neofetch.');
+    }
+  }
+};
